2fa4f4bd21fb01cfaf97acba85cdb0dfde0295ec
Rodrigo D. Cerdan <cerdanrodrigo.utn@gmail.com>
2020-04-01 21:51:11 -0300

    Sugerencia de fix sobre la aplicacion de los metodos de la clase ptseq

--- docs/assets/main.js
+++ docs/assets/main.js
@@ -1,102 +1,121 @@
-   let uno = document.getElementById("eq1");
-   let dos = document.getElementById("eq2");
-   let bt11 = document.getElementById("bt1-1");
-   let bt12 = document.getElementById("bt1-2");
-   let bt13 = document.getElementById("bt1-3");
-   let bt21 = document.getElementById("bt2-1");
-   let bt22 = document.getElementById("bt2-2");
-   let bt23 = document.getElementById("bt2-3");
-   let est1 = document.getElementById("est1");
-   let est2 = document.getElementById("est2");
-   let aux = uno.innerHTML;     
-   
-   class ptseq{
-   
-   constructor(p){
-      this.puntos=p;
-      this.buenas=false;
-      est1.innerHTML = `<h2 class="font-weight-light">Malas</h2>`;
-      est1.style.color = "#A30000";
-      est2.innerHTML = `<h2 class="font-weight-light">Malas</h2>`;
-      est2.style.color = "#A30000";
-   }
-  
-   iniciar(p){
-      return this.puntos=p;
-   }
-   restar(){
-      return this.puntos--;
-   }
-   sumar(){
-      return this.puntos++;
-   }
+let uno = document.getElementById("eq1");
+let dos = document.getElementById("eq2");
+let bt11 = document.getElementById("bt1-1");
+let bt12 = document.getElementById("bt1-2");
+let bt13 = document.getElementById("bt1-3");
+let bt21 = document.getElementById("bt2-1");
+let bt22 = document.getElementById("bt2-2");
+let bt23 = document.getElementById("bt2-3");
+let est1 = document.getElementById("est1"); //Estado Eq1
+let est2 = document.getElementById("est2"); //Estado Eq2
+let aux = uno.innerHTML;    
+
+
+class ptseq{
+
+constructor(p){
+   this.puntos=p;
+   this.buenas=false;
+   est1.innerHTML = `<h2 class="font-weight-light">Malas</h2>`;
+   est1.style.color = "#A30000";
+   est2.innerHTML = `<h2 class="font-weight-light">Malas</h2>`;
+   est2.style.color = "#A30000";
+}
+
+iniciar(p){
+   return this.puntos=p;
+}
+restar(){
+   return --this.puntos;
+}
+sumar(){
+   return this.puntos++;
+}
 }
 
 // Equipo 1
-const eq1 = new ptseq(0);
+const eq1 = new ptseq(1);
 bt13.onclick = function(){ 
-   uno.innerHTML = eq1.iniciar(0);
-   est1.innerHTML = `<h2 class="font-weight-light">Malas</h2>`;
-   est1.style.color = "#A30000";
-   eq1.buenas=false;
+uno.innerHTML = eq1.iniciar(0);
+est1.innerHTML = `<h2 class="font-weight-light">Malas</h2>`;
+est1.style.color = "#A30000";
+eq1.buenas=false;
 }
 bt11.onclick = function(){
-   if (eq1.puntos <=16){
-      uno.innerHTML = eq1.sumar();
-      
-      if (eq1.puntos ==16 && eq1.buenas==false){
-      eq1.buenas = true;
-      est1.innerHTML = `<h2 class="font-weight-light">Buenas</h2>`;
-      est1.style.color = "#218838";
-      eq1.puntos=0;
-      }
-   }
+console.log('suma ', eq1.puntos);
+if (eq1.puntos <=16){
+   uno.innerHTML = eq1.sumar();
    
+   if (eq1.puntos > 16 && eq1.buenas==false){
+         uno.innerHTML = eq1.iniciar(1);
+         eq1.puntos++;
+         eq1.buenas = true;
+         est1.innerHTML = `<h2 class="font-weight-light">Buenas</h2>`;
+         est1.style.color = "#218838";
+   }
+   if (eq1.puntos == 16 & eq1.buenas==true){
+      bt11.disabled=true;
+   }
+   if (eq1.puntos > 0 & eq1.buenas==false){
+      bt12.disabled=false;
+   }
 }
-bt12.onclick = function(){
-   if(eq1.puntos>=0){
-      uno.innerHTML = eq1.restar();
+return eq1.puntos;
+}
 
-      if(eq1.puntos==0 && eq1.buenas==true){
+//$('#bt1-2').off().on('click', function(){
+bt12.onclick = function(){
+console.log('resta ', eq1.puntos);
+
+if (eq1.puntos == 1 & eq1.buenas==false){
+   bt12.disabled=true;
+}
+if (eq1.puntos == 16 & eq1.buenas==true){
+   bt11.disabled=false;
+}
+if(eq1.puntos>=0){
+   uno.innerHTML = eq1.restar();
+   if(eq1.puntos==0 && eq1.buenas==true){
+      uno.innerHTML = 15
       eq1.buenas = false;
       est1.innerHTML = `<h2 class="font-weight-light">Malas</h2>`;
       est1.style.color = "#A30000";
       eq1.puntos=15;
-      } 
-   }  
-}
+   }
+}  
+};
 // Equipo 2
 const eq2 = new ptseq(0);
 bt23.onclick = function(){ 
-   dos.innerHTML = eq2.iniciar(0);
-   est2.innerHTML = `<h2 class="font-weight-light">Malas</h2>`;
-   est2.style.color = "#A30000";
-   eq2.buenas=false;
+dos.innerHTML = eq2.iniciar(0);
+est2.innerHTML = `<h2 class="font-weight-light">Malas</h2>`;
+est2.style.color = "#A30000";
+eq2.buenas=false;
 }
 bt21.onclick = function(){
-   if (eq2.puntos <=16){
-      dos.innerHTML = eq2.sumar();
-      
-      if (eq2.puntos ==16 && eq2.buenas==false){
-      eq2.buenas = true;
-      est2.innerHTML = `<h2 class="font-weight-light">Buenas</h2>`;
-      est2.style.color = "#218838";
-      eq2.puntos=0;
-      }
-   }
+if (eq2.puntos <=16){
+   dos.innerHTML = eq2.sumar();
    
+   if (eq2.puntos ==16 && eq2.buenas==false){
+   eq2.buenas = true;
+   est2.innerHTML = `<h2 class="font-weight-light">Buenas</h2>`;
+   est2.style.color = "#218838";
+   eq2.puntos=0;
+   }
+}
+
 }
 bt22.onclick = function(){
-   if(eq2.puntos>=0){
-      dos.innerHTML = eq2.restar();
+if(eq2.puntos>=0){
+   dos.innerHTML = eq2.restar();
 
-      if(eq2.puntos==0 && eq2.buenas==true){
-      eq2.buenas = false;
-      est2.innerHTML = `<h2 class="font-weight-light">Malas</h2>`;
-      est2.style.color = "#A30000";
-      eq2.puntos=15;
-      } 
-   }  
+   if(eq2.puntos==0 && eq2.buenas==true){
+   eq2.buenas = false;
+   est2.innerHTML = `<h2 class="font-weight-light">Malas</h2>`;
+   est2.style.color = "#A30000";
+   eq2.puntos=15;
+   } 
+}  
 }
 // bt23.onclick = function(){ 
 //    dos.innerHTML = eq2.iniciar(0);
